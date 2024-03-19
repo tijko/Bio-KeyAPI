0715f53a2896d9001f5c38c7b0c50ecbdd612084
Tim Konick <konick781@gmail.com>
2024-03-19 18:36:05 -0400

    Fix KeyError

--- BioKey-Tests.py
+++ BioKey-Tests.py
@@ -37,11 +37,9 @@ def test_get_users(client):
     response = client.get('/items')
     assert response.status_code == 200
     data = json.loads(response.data.decode('utf-8'))
-    print(data)
-    assert len(data) == 1  # Assuming there are 3 users in the database
-
+    assert len(data) == 1
     # You can add more assertions based on the expected response data
-    #assert data[0]['items'][0]['name'] == 'Tim'
+    assert data['items'][0]['name'] == 'Tim'
     #assert data[1]['name'] == 'Bob'
     #assert data[2]['name'] == 'Charlie'
 
