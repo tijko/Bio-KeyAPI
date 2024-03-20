dd03e8aea35138004ce0f0359deb5683d497c255
Tim Konick <konick781@gmail.com>
2024-03-20 14:42:15 -0400

    Directory Adjustment

--- BioKey.py
+++ BioKey.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Function to establish a connection with the SQLite database
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('db/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
