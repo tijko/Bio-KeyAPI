8272ce34e73a6be173ff56d2bada3277f193b7d0
Tim Konick <konick781@gmail.com>
2024-03-20 14:51:09 -0400

    Order Tests

--- BioKey-Tests.py
+++ BioKey-Tests.py
@@ -45,6 +45,11 @@ def client():
         client.put('/item/2', data=item_two, headers={'Content-Type':'application/json'})
         yield client
 
+def test_get_itemid(client):
+    response = client.get('/item/2')
+    entry = json.loads(response.data.decode('utf-8'))
+    assert entry['id'] == 2
+
 def test_get_items(client):
     response = client.get('/items')
     assert response.status_code == 200
@@ -59,11 +64,6 @@ def test_put_item(client):
                            headers={'Content-Type':'application/json'},
                          )
     assert 200 == response.status_code
-
-def test_get_itemid(client):
-    response = client.get('/item/2')
-    entry = json.loads(response.data.decode('utf-8'))
-    assert entry['id'] == 2
 
 def test_delete_itemid(client):
     response = client.delete('/item/2')
